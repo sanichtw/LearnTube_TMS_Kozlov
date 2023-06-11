998652138822cd7ddb256081ef4ece4e4f2f1da7
Nikita Kozlov <sanichtw@gmail.com>
2023-06-11 19:44:56 +0300

    add password and mail verification logic, error textView

--- app/src/main/java/com/example/learntube/presentation/fragments/LoginScreen.kt
+++ app/src/main/java/com/example/learntube/presentation/fragments/LoginScreen.kt
@@ -1,10 +1,13 @@
 package com.example.learntube.presentation.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.learntube.R
 import com.example.learntube.databinding.FragmentLoginScreenBinding
@@ -15,10 +18,11 @@ class LoginScreen : Fragment() {
     private var _binding: FragmentLoginScreenBinding? = null
     private val binding get() = _binding!!
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentLoginScreenBinding.inflate(inflater, container, false)
         return binding.root
@@ -28,13 +32,71 @@ class LoginScreen : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setListenersToInputs()
+
         binding.loginButton.setOnClickListener {
-            findNavController().navigate(R.id.action_LoginScreen_to_SecondFragment)
+            val email = binding.emailInput.text.toString().trim()
+            val password = binding.passwordInput.text.toString().trim()
+            val errorText = binding.emailErrorTextView
+            val loginButton = binding.loginButton
+
+            if (email.isEmpty() || password.isEmpty()) {
+                errorText.apply {
+                    text = "Please enter email and password"
+                    visibility = View.VISIBLE
+                }
+            } else {
+                errorText.visibility = View.GONE
+                loginButton.isEnabled = true
+                findNavController().navigate(R.id.action_LoginScreen_to_SecondFragment)
+            }
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun setListenersToInputs() {
+        binding.emailInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                updateButtonState()
+            }
+        })
+
+        binding.passwordInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                updateButtonState()
+            }
+        })
+    }
+
+    private fun updateButtonState() {
+        val email = binding.emailInput.text.toString().trim()
+        val password = binding.passwordInput.text.toString().trim()
+        val errorText = binding.emailErrorTextView
+        val loginButton = binding.loginButton
+
+        val emailValid = validateEmail(email)
+        val passwordValid = validatePassword(password)
+
+        loginButton.isEnabled = emailValid && passwordValid
+
+        if (!emailValid || !passwordValid) {
+            errorText.apply {
+                text = "Incorrect password or email"
+                visibility = View.VISIBLE
+            }
+        } else {
+            errorText.visibility = View.GONE
+        }
+    }
+
+    private fun validateEmail(email: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    private fun validatePassword(password: String): Boolean {
+        return password.length >= 8
     }
 }
