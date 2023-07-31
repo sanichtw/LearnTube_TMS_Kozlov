2082f8cf131e481993c6ac44d7dc467ab49d2d6d
Nikita Kozlov <sanichtw@gmail.com>
2023-07-31 19:50:00 +0300

    added splash screen

---
+++ app/src/main/java/com/example/learntube/presentation/activities/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.example.learntube.presentation.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.learntube.R
+
+internal class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_SCREEN_TIMEOUT)
+    }
+    companion object {
+        const val SPLASH_SCREEN_TIMEOUT: Long = 1500
+    }
+}
